b49a1c92ddabc7151e3bb8414cb2cb4b93c7f244
Trent <trent.gill@gmail.com>
2017-06-14 00:53:37 -0400

    rm parsec. implemented WORD parser directly in haskell

--- app/Main.hs
+++ app/Main.hs
@@ -3,17 +3,12 @@ module Main where
 --import Lib
 import System.Environment
 import Control.Monad
-import Text.ParserCombinators.Parsec hiding (spaces)
 import Data.List
 
 main :: IO ()
 main = do
   (expr:_) <- getArgs
   putStrLn (expr)
-  --(main shortstr)
-
---symbol :: Parser Char
---symbol = oneOf 
 
 -- DICTIONARY
 -- WORD
