7bfa2a41326b69ad9f0a87f1ba0c2391d5155ae7
Trent <Air@Wind.local>
2017-06-23 20:11:15 -0400

    BL WORD and FIND successfully working though WORD doesn't drop BL from stack

--- app/Main.hs
+++ app/Main.hs
@@ -4,18 +4,26 @@ module Main where
 import System.Environment
 import Control.Monad
 import Data.List
+import Control.Arrow
+import Data.Char
 
 main :: IO ()
 main = do
     interpret_this <- getLine
-    print(fINTERPRET interpret_this [])
-    main
+    let initState = FState {
+                    datastack = [],
+                    input_string = interpret_this,
+                    output_string = "" }
+    let endState = fQUIT initState
+    print( endState )
+    --print( output_string endState ) -- only the output str
+    --main
     
-
-
+        --input_string = "7 FIVE SQUARED BYE",
+    --print(fINTERPRET interpret_this [])
     --print (fSQUARED      -- 49
     --     . fSEVEN        -- 7
-    --     $ [] :: FStack) -- empty stack
+    --     $ [] :: FDataStack) -- empty stack
     ----putStrLn (name)
 
 -- TYPES
@@ -28,70 +36,113 @@ main = do
     -- Print string
     -- Dictionary(?)
 
--- empty stack & string to be interp'd
--- initState = FState [] "5 SQUARED BYE"
+data FState = FState
+  { datastack :: FDataStack
+  , input_string :: FInput
+  , output_string :: FOutput }
+  deriving (Show)
 
---data FState =
---    FState { stack :: FStack
---           , instr :: FInput }
-
--- Input String (type alias)
+-- Type aliases
 type FInput = String
+type FOutput = String
+type FDataStack = [FStackItem]
 
-
--- Data Stack
--- type alias, not an ADT
-type FStack = [FStackItem]
-
--- define all the things that can be elements of the stack
+-- All potential stack contents (XT etc)
 data FStackItem = FNum Integer
                 | FStr String
-                deriving (Eq, Ord, Read)
+                | FFn  (FState -> FState)
 
 instance Show FStackItem where
     show (FNum x) = show x
     show (FStr x) = x
-
-
+    show (FFn  x) = "<function>"
 
 -- DICTIONARY
 
---interpret loop in haskell is different to forth
---input is the input string from the repl
---output is 'printable' as returned by functions
---for now we just return the FStack and print the contents
---later implement DOT and DOTESS to change the 'printable' state in FState
 
---nb: this should take FState input for persistance across repl cycles
---currently, each line operates in isolation
---need the StateT monad
+-- empty return stack
+-- accept a line of input from terminal
+-- interpret input buffer til empty
+-- repeta from 'accept'
+fQUIT :: FState -> FState
+fQUIT = fEmptyRS
+    >>> fINTER
+-- should throw loop back through main now
 
-fINTERPRET :: String -> FStack -> FStack
+fEmptyRS :: FState -> FState
+fEmptyRS st = st
+-- eventually need to clear the return stack after it's implemented
+
+fINTER :: FState -> FState
+fINTER stay@(FState {input_string=[]}) = stay
+fINTER stay = fINTER . fFIND . fWORD . fBL $ stay
+
+fINTERPRET :: String -> FDataStack -> FDataStack
 fINTERPRET []  stk = stk -- base case, return stack state
-fINTERPRET str stk = fINTERPRET str' $ fIPTWO (FStr word):stk
+fINTERPRET str stk = fINTERPRET str' $ stk
     where word = takeWhile (/= ' ') (str)
           str' = drop (1 + length word) str
 
--- interpret without any side-effects
-fIPTWO :: FStack -> FStack
-fIPTWO
 --Interpret loop in Forth
 --BL WORD FIND
 --IF EXECUTE
 --ELSE >TONUM
 --THEN
 
+fWORD :: FState -> FState
+fWORD s = s { datastack = (FStr word):(datastack s)
+            , input_string = str' }
+    where word  = takeWhile (/= delim) (input_string s)
+          str'  = drop (1 + length word) (input_string s)
+          delim = ' '
+          --delim = head(head(datastack s)) :: Char
 
---fWORD :: FStack
+-- pattern match into the dictionary here!
+fFIND :: FState -> FState
+fFIND s = s { datastack = dFIND (datastack s) } where 
+    dFIND []          = []
+    dFIND (FStr s:ss)
+            | s == "FIVE"   = (FFn fFIVE):ss
+            | s == "DUP"    = (FFn fDUP):ss
+            | s == "*"      = (FFn fSTAR):ss
+            | s == "SQUARED"= (FFn fSQUARED):ss
+            | otherwise     = (FNum (toInteger (digitToInt $ head s))):ss
+                -- nb: ^error case
 
---fWORD :: FStack -> FStack
+fEXECUTE :: FState -> FState
+fEXECUTE s = 
+
+
+
+-- having trouble here:
+-- how to destructively pop the stack for the delim
+    -- then pass the modified stack to 'word' to append?
+
+--fWORD :: FState -> FState
+--fWORD s = s { datastack = dWORD (datastack s, input_string s),
+--              input_string = str' }
+--    let dWORD = 
+
+--intWord :: FDataStack -> FInput -> (FDataStack, FInput)
+--intWord stack string = stack' string'
+--    where stack' = (FStr word):stack
+--    where string'= 
+
+
+--fBL :: FState -> FState
+--fBL s = s { datastack = dBL (datastack s) }
+--    where dBL s = FStr " " : s
+
+--fWORD :: FDataStack
+
+--fWORD :: FDataStack -> FDataStack
 --fWORD (delim:inStr:st) = (word, shortstr)
   --where word           = takeWhile (/= delim) (inStr)
         --shortstr       = drop (1 + length word) inStr
 
 
 
---fDOLITERAL :: FPC -> FStack -> FStack
+--fDOLITERAL :: FPC -> FDataStack -> FDataStack
 --fDOT
 --fBYE
 
@@ -118,80 +169,84 @@ fIPTWO
 --Console I/O: KEY EMIT
 
 --LITERALS / CONSTANTS
-fFIVE :: FStack -> FStack
-fFIVE st = (FNum 5):st
+fFIVE :: FState -> FState
+fFIVE s = s { datastack = dFive (datastack s) }
+    where dFive st = (FNum 5):st
 
-fSEVEN :: FStack -> FStack
-fSEVEN st = (FNum 7):st
+fBL :: FState -> FState
+fBL s = s { datastack = dBL (datastack s) }
+    where dBL s = FStr " " : s
 
-fBL :: FStack -> FStack
-fBL st = (FStr " "):st
-
+--dBL :: FDataStack -> FDataStack
+--dBL s = FStr " " : s
 
 
 --MATH FUNCTIONS
-fSTAR :: FStack -> FStack
-fSTAR (FNum s:FNum st:stk) = FNum(s * st) : stk
+fSTAR :: FState -> FState
+fSTAR s = s { datastack = dStar (datastack s) }
+    where dStar (FNum s:FNum st:stk) = FNum(s * st) : stk
 
-fADD :: FStack -> FStack
+
+fADD :: FDataStack -> FDataStack
 fADD (FNum s:FNum st:stk) = FNum(s + st) : stk
 
-fSUB :: FStack -> FStack
+fSUB :: FDataStack -> FDataStack
 fSUB (FNum s:FNum st:stk) = FNum(s - st) : stk
 
-fDIV :: FStack -> FStack
+fDIV :: FDataStack -> FDataStack
 fDIV (FNum s:FNum st:stk) = FNum(div s st) : stk
 
-fMAX :: FStack -> FStack
-fMAX (s:st:stk) = max s st : stk
+fMAX :: FDataStack -> FDataStack
+fMAX (FNum s:FNum st:stk) = FNum(max s st) : stk
 
-fMIN :: FStack -> FStack
-fMIN (s:st:stk) = min s st : stk
+fMIN :: FDataStack -> FDataStack
+fMIN (FNum s:FNum st:stk) = FNum(min s st) : stk
 
 
 
 --PRINTING
-fDOT :: FStack -> FStack
+fDOT :: FDataStack -> FDataStack
 fDOT []      = []
 --fDOT (s:stk) = stk, print(s)
 
-fTONUM :: FStack -> FStack
-fTONUM ((FStr s):st) = (read s):st
+--fTONUM :: FDataStack -> FDataStack
+--fTONUM ((FStr s):st) = (read s):st
 -- protect against non-numerals..
 
 
 
 --STACK MODIFIERS
-fDUP :: FStack -> FStack
-fDUP []        = []
-fDUP (tos:stk) = tos:tos:stk
+fDUP :: FState -> FState
+fDUP s = s { datastack = dDup (datastack s) }
+    where dDup []        = []
+          dDup (tos:stk) = tos:tos:stk
 
-fDROP :: FStack -> FStack
+fDROP :: FDataStack -> FDataStack
 fDROP []      = []
 fDROP (_:stk) = stk
 
-fSWAP :: FStack -> FStack
+fSWAP :: FDataStack -> FDataStack
 fSWAP []            = []
 fSWAP (tos:[])      = tos:[]
 fSWAP (tos:nxt:stk) = nxt:tos:stk
 
-fOVER :: FStack -> FStack
+fOVER :: FDataStack -> FDataStack
 fOVER []            = []
 fOVER (tos:[])      = tos:[]
 fOVER (tos:nxt:stk) = nxt:tos:nxt:stk
 
-fROT :: FStack -> FStack
+fROT :: FDataStack -> FDataStack
 fROT []                = []
 fROT (tos:[])          = tos:[]
 fROT (tos:nxt:[])      = tos:nxt:[]
 fROT (tos:nxt:thd:stk) = thd:tos:nxt:stk
 
-fNIP :: FStack -> FStack
+fNIP :: FDataStack -> FDataStack
 fNIP []          = []
 fNIP (tos:[])    = tos:[]
 fNIP (tos:_:stk) = tos:stk
 
-fTUCK :: FStack -> FStack
+fTUCK :: FDataStack -> FDataStack
 fTUCK []            = []
 fTUCK (tos:[])      = tos:tos:[]
 fTUCK (tos:nxt:stk) = tos:nxt:tos:stk
@@ -199,17 +254,20 @@ fTUCK (tos:nxt:stk) = tos:nxt:tos:stk
 
 
 --COMPOSITE WORDS (hand compiled)
-fSQUARED :: FStack -> FStack
-fSQUARED stk = fSTAR . fDUP $ stk
+fSQUARED :: FState -> FState
+fSQUARED = fDUP
+       >>> fSTAR
 
---fINIT :: FStack -> FStack
+
+
+--fINIT :: FDataStack -> FDataStack
 --fINIT stk = fBYE
 --          . fSQUARED
 --          . 42
 --          . DOLITERAL
 --          $ stk
 
---fQDUP :: FStack -> FStack
+--fQDUP :: FDataStack -> FDataStack
 --fQDUP ((FNum s):st)
 --    | s /= 0 = s:s:st
 --    | otherwise = s:st
